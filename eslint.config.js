36e28348102412009e8900a96466730ebefdbbb3
Maksim Shkryabin <maks4k32rus@gmail.com>
2024-11-06 14:20:28 +0300

    создали первый комит

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+
+export default[
+    js.configs.recommended,
+    prettier,
+    {
+        files:["src/**/*.js"],
+        plugins:{
+            prettier:prettier.rules
+        },
+        rules:{
+            "no-unused-vars": "warn",//правило не использовать переменные var
+        },
+    },
+]
